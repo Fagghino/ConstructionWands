b68b583b4fbd43b3590aa543a8a09a26e724d132
Fagghino <fgiordano839@gmail.com>
2025-10-31 19:47:38 +0100

    Update wand command to support player targeting and quantity parameter

--- src/main/java/com/franchino961/constructionwands/commands/WandCommand.java
+++ src/main/java/com/franchino961/constructionwands/commands/WandCommand.java
@@ -20,39 +20,73 @@ public class WandCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "Solo i giocatori possono eseguire questo comando!");
-            return true;
-        }
-
-        Player player = (Player) sender;
-
-        if (!player.hasPermission("constructionwands.give")) {
-            String noPermMsg = plugin.getConfig().getString("messages.no-permission", "&cNon hai il permesso per usare questo comando!");
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', noPermMsg));
-            return true;
-        }
-
         if (args.length < 1) {
-            player.sendMessage(ChatColor.RED + "Uso: /wand <nome_bacchetta>");
-            player.sendMessage(ChatColor.YELLOW + "Bacchette disponibili: " +
+            sender.sendMessage(ChatColor.RED + "Uso: /wand <nome_bacchetta> [nome_player] [quantità]");
+            sender.sendMessage(ChatColor.YELLOW + "Bacchette disponibili: " +
                     String.join(", ", wandManager.getAllWands().keySet()));
             return true;
         }
-        String wandId = args[0];
-        ItemStack wandItem = wandManager.createWandItem(wandId);
 
-        if (wandItem == null) {
-            String notFoundMsg = plugin.getConfig().getString("messages.wand-not-found", "&cBacchetta non trovata!");
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', notFoundMsg));
+        String wandId = args[0];
+        String playerName = null;
+        int quantity = 1;
+
+        if (args.length >= 2) {
+            playerName = args[1];
+        }
+        if (args.length >= 3) {
+            try {
+                quantity = Integer.parseInt(args[2]);
+                if (quantity < 1) quantity = 1;
+            } catch (NumberFormatException e) {
+                sender.sendMessage(ChatColor.RED + "Quantità non valida!");
+                return true;
+            }
+        }
+
+        Player targetPlayer;
+        if (playerName != null) {
+            targetPlayer = plugin.getServer().getPlayer(playerName);
+            if (targetPlayer == null) {
+                sender.sendMessage(ChatColor.RED + "Giocatore non trovato!");
+                return true;
+            }
+        } else {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(ChatColor.RED + "Devi specificare un nome giocatore quando esegui da console!");
+                return true;
+            }
+            targetPlayer = (Player) sender;
+        }
+
+        if (!sender.hasPermission("constructionwands.give")) {
+            String noPermMsg = plugin.getConfig().getString("messages.no-permission", "&cNon hai il permesso per usare questo comando!");
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', noPermMsg));
             return true;
         }
 
-        player.getInventory().addItem(wandItem);
+        ItemStack wandItem = wandManager.createWandItem(wandId);
+        if (wandItem == null) {
+            String notFoundMsg = plugin.getConfig().getString("messages.wand-not-found", "&cBacchetta non trovata!");
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', notFoundMsg));
+            return true;
+        }
+
+        for (int i = 0; i < quantity; i++) {
+            targetPlayer.getInventory().addItem(wandItem.clone());
+        }
 
         String givenMsg = plugin.getConfig().getString("messages.wand-given", "&aHai ricevuto una {wand}!");
         givenMsg = givenMsg.replace("{wand}", wandManager.getWand(wandId).getName());
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', givenMsg));
+        if (quantity > 1) {
+            givenMsg += " x" + quantity;
+        }
+        targetPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', givenMsg));
+
+        if (!sender.equals(targetPlayer)) {
+            sender.sendMessage(ChatColor.GREEN + "Hai dato " + wandManager.getWand(wandId).getName() + " a " + targetPlayer.getName() + (quantity > 1 ? " x" + quantity : ""));
+        }
+
         return true;
     }
 
@@ -61,6 +95,16 @@ public class WandCommand implements CommandExecutor, TabCompleter {
         List<String> completions = new ArrayList<>();
         if (args.length == 1) {
             completions.addAll(wandManager.getAllWands().keySet());
+        } else if (args.length == 2) {
+            // Suggerisci giocatori online
+            for (Player player : plugin.getServer().getOnlinePlayers()) {
+                completions.add(player.getName());
+            }
+        } else if (args.length == 3) {
+            // Suggerisci quantità
+            completions.add("1");
+            completions.add("5");
+            completions.add("10");
         }
         return completions;
     }
